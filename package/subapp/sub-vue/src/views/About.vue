1171f4b2fdbe2ed4acddc9721617ec3b0611ae99
张炷远 <zhangzhuyuan@c2matica.com>
2022-05-16 13:58:42 +0800

    '测试element弹窗是否可以全屏'

--- package/subapp/sub-vue/src/views/About.vue
+++ package/subapp/sub-vue/src/views/About.vue
@@ -1,7 +1,16 @@
 <template>
   <h1>这是about页面</h1>
+  <el-button @click="dialogVisible = true">打开弹窗</el-button>
+  <el-dialog title="弹窗" v-model="dialogVisible">
+    <p>这是弹窗内容</p>
+    <el-button @click="dialogVisible = false">关闭</el-button>
+  </el-dialog>
 </template>
 
-<script setup></script>
+<script setup>
+// import { ref } from 'vue'
+
+const dialogVisible = ref(false)
+</script>
 
 <style scoped></style>
